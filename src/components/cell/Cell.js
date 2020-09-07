ada035ed34842bbd2eaa4efa0c6ff00aa8d0b73d
Renato Melo <renatoferreira249@gmail.com>
2020-09-07 20:44:21 -0300

    delete all feature

--- src/components/cell/Cell.js
+++ src/components/cell/Cell.js
@@ -3,12 +3,20 @@ import style from './Cell.module.scss';
 import Reminder from 'Components/reminder/Reminder';
 import dayjs from 'dayjs';
 import ReminderContext from 'Contexts/Context';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 function Cell(props) {
-  const { months } = useContext(ReminderContext);
+  const { months, setMonths } = useContext(ReminderContext);
   const month = props.day.get('month');
   const date = props.day.get('date');
   const reminders = months[month][ date - 1];
+
+  const deleteAll = () => {
+    const updatedMonths = JSON.parse(JSON.stringify(months));
+    updatedMonths[month][ date - 1] = [];
+    setMonths(updatedMonths);
+
+  }
 
 	return(
 		<div id={`r-${month}-${date}`} className={style['cell']}>
@@ -20,6 +28,7 @@ function Cell(props) {
            reminders.map( (r, i) => <Reminder position={i} reminder={r}/> )
            )}
       </div>
+      <DeleteIcon onClick={deleteAll}/>
 		</div>
 	);
 }
